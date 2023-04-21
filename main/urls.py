34724aed602e9a0155a0bd9063f8883093f1e202
Emily <emilymartin413@gmail.com>
2023-04-21 18:30:40 -0500

    User page error hunt

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 from register import views as v
+from . import views
+from django.urls import path
+from .views import user_page
+
+urlpatterns = [
+    # other URL patterns here...
+    path('user_page/', user_page, name='user_page'),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("register/", v.register, name="register"),
-    path('', include("main.urls")),
     path('', include("django.contrib.auth.urls")),
+    path("<int:id>", views.index, name="index"),
+    path("", views.home, name="home.html"),
+    path("view/", views.view, name="view"),
+
 ]
